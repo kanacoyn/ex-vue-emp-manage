c2f8e8a552bd617e6752c96fc2b206fa331e53f3
kanacoyn <knkyn723@gmail.com>
2022-02-09 11:43:00 +0900

    画面遷移を設定するファイル

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,8 +6,8 @@ Vue.use(VueRouter);
 const routes: Array<RouteConfig> = [
   // // パスが登録されていない場合はログイン画面へ(LoginAdmin.vueを作成したらコメントを外してください)
   // {
-  //   path: "*",
-  //   component: () => import("../components/LoginAdmin.vue"),
+    path: "/registerAdmin",
+    component: () => import("../components/RegisterAdmin.vue"),
   // },
 ];
 
