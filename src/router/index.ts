f72c42f2e5f22f176cc5b22808a588324086c140
kanacoyn <knkyn723@gmail.com>
2022-02-09 11:44:16 +0900

    管理者登録機能vueファイルを作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,10 +5,10 @@ Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   // // パスが登録されていない場合はログイン画面へ(LoginAdmin.vueを作成したらコメントを外してください)
-  // {
+  {
     path: "/registerAdmin",
     component: () => import("../components/RegisterAdmin.vue"),
-  // },
+  },
 ];
 
 const router = new VueRouter({
