35fba9c600bcf413404b38131f78078eccd9e426
kanacoyn <knkyn723@gmail.com>
2022-02-10 16:31:46 +0900

    終了

---
+++ src/views/LogoutAdmin.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    ログアウト中・・・
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+@Component
+export default class LogoutAdmin extends Vue {
+  /**
+   * ログアウトする.
+   *
+   * @returns Promiseオブジェクト
+   */
+  async created(): Promise<void> {
+    const response = await axios.get(
+      "http://153.127.48.168:8080/ex-emp-api/logout",
+      {}
+    );
+    console.dir("response:" + JSON.stringify(response));
+    if (response.data.status === "success") {
+      this.$router.push("/loginAdmin");
+    }
+  }
+}
+</script>
+
+<style scoped></style>
