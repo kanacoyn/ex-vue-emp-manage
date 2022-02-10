14e98748fc6452b5a2dfd881c3bb8f5861cf18bc
kanacoyn <knkyn723@gmail.com>
2022-02-10 11:42:46 +0900

    従業員詳細ページのためにルーティング

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,6 +16,10 @@ const routes: Array<RouteConfig> = [
     path: "/employeeList",
     component: () => import("../views/EmployeeList.vue"),
   },
+  {
+    path: "/employeeDetail/:id",
+    component: () => import("../views/EmployeeDetail.vue"),
+  },
 ];
 
 const router = new VueRouter({
